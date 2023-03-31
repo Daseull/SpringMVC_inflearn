a1d6c0a954c6fb49c4b4784061e55046c441c598
Daseul <daseull.rep@gmail.com>
2023-03-31 14:02:10 +0900

    Spring MVC (old)
    * Spring은 DispatcherServlet, HandlerMapping, HandlerAdapter를 이미 가지고 있다.
    * 따라서 필요한 컨트롤러를 구현하고, 매핑할 url 정보를 지정해주면 서블릿이 핸들러 매핑, 어댑터 조회, 호출, 뷰리졸버 호출 등을 모두 해결해준다.

---
+++ servlet/src/main/java/hello/servlet/web/springmvc/old/MyHttpRequestHandler.java
@@ -0,0 +1,18 @@
+package hello.servlet.web.springmvc.old;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.HttpRequestHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+// DispatcherServlet이 이 핸들러를 찾을 수 있도록 스프링 빈의 이름을 url 패턴으로 지정
+@Component("/spring-mvc/request-handler")
+public class MyHttpRequestHandler implements HttpRequestHandler {
+    @Override
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MyHttpRequestHandler.handleRequest");
+    }
+}
